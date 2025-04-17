0dc5ffa7c9c257e8cc115153ba73679d39b743b7
Rayane ELOUDJEDI <reloudjedi@dev-id.fr>
2025-04-17 23:31:36 +0200

    fix: bypass bot detection

--- commands/play.ts
+++ commands/play.ts
@@ -108,7 +108,18 @@ function playSong(guildId: string, song: Song) {
   const resource = createAudioResource(ytdl(song.url, { 
     filter: 'audioonly',
     quality: 'highestaudio',
-    highWaterMark: 1 << 25
+    highWaterMark: 1 << 25,
+    requestOptions: {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
+      }
+    }
   }));
   
   player.play(resource);
